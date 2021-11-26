487e88953cab43bf76e81d1489c45dd2d7f15647
Malte Wächter <mawaechter@student.ethz.ch>
2021-11-26 11:31:25 +0100

    [Movement] Refactor: Introduce Stage interface and SmarAct implementation (#11)
    * Movement Refactor: New stage interface and implementation for SmarAct stage
    
    Last changes
    
    * Raise an error when MCS Control raises an error
    
    * Fix typos and make MovementType necessary of movement

---
+++ LabExT/Movement/Stage.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LabExT  Copyright (C) 2021  ETH Zurich and Polariton Technologies AG
+This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
+"""
+
+import logging
+from abc import ABC, abstractmethod
+
+
+class StageError(RuntimeError):
+    pass
+
+
+class Stage(ABC):
+    @abstractmethod
+    def __init__(self, address):
+        self.address = address
+        self.connected = False
+        self.logger = logging.getLogger()
+
+    def __del__(self):
+        self.disconnect()
+
+    @abstractmethod
+    def connect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> bool:
+        pass
+
+    @abstractmethod
+    def set_speed_xy(self, umps: float):
+        pass
+
+    @abstractmethod
+    def set_speed_z(self, umps: float):
+        pass
+
+    @abstractmethod
+    def get_speed_xy(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_speed_z(self) -> float:
+        pass
+
+    @abstractmethod
+    def set_acceleration_xy(self):
+        pass
+
+    @abstractmethod
+    def get_acceleration_xy(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_status(self) -> tuple:
+        pass
+
+    @abstractmethod
+    def wiggle_z_axis_positioner(self):
+        pass
+
+    @abstractmethod
+    def lift_stage(self):
+        pass
+
+    @abstractmethod
+    def lower_stage(self):
+        pass
+
+    @abstractmethod
+    def get_current_position(self):
+        pass
+
+    @abstractmethod
+    def move_relative(self, x, y):
+        pass
+
+    @abstractmethod
+    def move_absolute(self, pos):
+        pass
