893852aa39555948d5b34f31e688db40e08f06f9
Malte Wächter <maltewaechter1@gmail.com>
2022-02-16 17:48:09 +0100

    Movement Update: Configuration Wizard and tests
    Adds MoverNew and Calibration classes and the corresponding MovementWizard configuration GUI.

--- LabExT/Movement/Calibration.py
+++ LabExT/Movement/Calibration.py
@@ -5,15 +5,44 @@ LabExT  Copyright (C) 2022  ETH Zurich and Polariton Technologies AG
 This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
 """
 
-from typing import Type
-
-from LabExT.Movement.config import DevicePort, Orientation, State
-from LabExT.Movement.Transformations import SinglePointOffset, AxesRotation, KabschRotation
+from enum import Enum, auto
 from LabExT.Movement.Stage import StageError
 
 
-class CalibrationError(RuntimeError):
-    pass
+class Orientation(Enum):
+    """
+    Enumerate different state orientations.
+    """
+    LEFT = auto()
+    RIGHT = auto()
+    TOP = auto()
+    BOTTOM = auto()
+
+    def __str__(self) -> str:
+        return self.name.capitalize()
+
+
+class DevicePort(Enum):
+    """Enumerate different device ports."""
+    INPUT = auto()
+    OUTPUT = auto()
+
+    def __str__(self) -> str:
+        return self.name.capitalize()
+
+
+class State(Enum):
+    """
+    Enumerate different calibration states.
+    """
+    UNINITIALIZED = 0
+    CONNECTED = 1
+    COORDINATE_SYSTEM_FIXED = 2
+    SINGLE_POINT_FIXED = 3
+    FULLY_CALIBRATED = 4
+
+    def __str__(self) -> str:
+        return self.name.replace('_', ' ').capitalize()
 
 
 class Calibration:
@@ -28,10 +57,6 @@ class Calibration:
         self._state = State.CONNECTED if stage.connected else State.UNINITIALIZED
         self._orientation = orientation
         self._device_port = device_port
-
-        self._axes_rotation: Type[AxesRotation] = None
-        self._single_point_offset: Type[SinglePointOffset] = None
-        self._kabsch_rotation: Type[KabschRotation] = None
 
     #
     #   Representation
