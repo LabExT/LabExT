ff774b12f4283050ce8201ce61ccdb6080431cbc
Malte Wächter <git@malte-waechter.com>
2022-05-14 14:07:46 +0200

    Add Menu Point for stage calibration

--- LabExT/View/MainWindow/MainWindowView.py
+++ LabExT/View/MainWindow/MainWindowView.py
@@ -64,6 +64,9 @@ class MainWindowContextMenu(Menu):
         self._movement_new.add_command(
             label="Configure Mover...",
             command=self._menu_listener.client_setup_mover)
+        self._movement_new.add_command(
+            label="Calibrate Stages...",
+            command=self._menu_listener.client_calibrate_stage)
 
         self._movement.add_command(
             label="Configure Stages",
