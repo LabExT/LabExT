1c1c4d69254122b91bf7f00cbad131ac6d45a421
Malte Wächter <git@malte-waechter.com>
2022-05-25 09:01:58 +0200

    Add Potential Field

--- LabExT/Movement/PathPlanning.py
+++ LabExT/Movement/PathPlanning.py
@@ -6,12 +6,15 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 """
 
 from abc import ABC, abstractmethod
-from typing import Tuple, Type
+from typing import TYPE_CHECKING, List, Tuple, Type
 
 import numpy as np
 
 from LabExT.Movement.Transformations import ChipCoordinate
 from LabExT.Movement.config import Orientation
+
+if TYPE_CHECKING:
+    from LabExT.Movement.Calibration import Calibration
 
 
 class StagePolygon(ABC):
@@ -154,3 +157,158 @@ class SingleModeFiber(StagePolygon):
             y_min -= grid_size / 2 + grid_epsilon
 
         return x_min, x_max, y_min, y_max
+
+
+class PotentialField:
+    """
+    Waypoint calculation with potential field algorithm.
+    A potential field is created for each stage, with all other stages being obstacles.
+
+    This algorithm operates in 2D (x,y), assuming that the stages are on a plane.
+    """
+
+    REPULSIVE_GAIN = 100.0
+    ATTRACTIVE_GAIN = 5.0
+    FIBER_RADIUS = 125.0
+
+    MOTIONS = [[1, 0],
+               [0, 1],
+               [-1, 0],
+               [0, -1],
+               [-1, -1],
+               [-1, 1],
+               [1, -1],
+               [1, 1]]
+
+    def __init__(
+        self,
+        calibration: Type["Calibration"],
+        target_position: Type[ChipCoordinate],
+        grid_size: float = 50.0,
+        grid_outline: tuple = ((-5000, 5000), (-5000, 5000))
+    ) -> None:
+        """
+        Constructor for new Potential Field
+
+        Parameters
+        ----------
+        calibration: Calibration
+            Instance of a calibration
+        target_coordinate: ChipCoordinate
+            Target in chip coordinates
+        grid_size: float
+            Size of the potential field grid
+        grid_outline: tuple
+            Outline (x_min, x_max, y_min, y_max) of the potential field grid
+        """
+        self.calibration = calibration
+        self.grid_size = grid_size
+        self.grid_outline = grid_outline
+
+        self.target_position = target_position
+
+        self.x_coords = np.arange(
+            self.grid_outline[0][0],
+            self.grid_outline[0][1] +
+            self.grid_size,
+            self.grid_size)
+        self.y_coords = np.arange(
+            self.grid_outline[1][0],
+            self.grid_outline[1][1] +
+            self.grid_size,
+            self.grid_size)
+        self.cx, self.cy = np.meshgrid(self.x_coords, self.y_coords)
+
+        self.attractive_potenial_field = self.ATTRACTIVE_GAIN * \
+            np.hypot(self.cx - self.target_position.x, self.cy - self.target_position.y)
+        self.potential_field = np.zeros_like(
+            self.cx) + self.attractive_potenial_field
+
+        with self.calibration.perform_in_chip_coordinates():
+            self.start_coordinate = self.calibration.get_position()
+            self.target_position.z = self.start_coordinate.z
+
+            self.current_idx = np.array(
+                [np.argmin(np.abs(self.x_coords - self.start_coordinate.x)),
+                 np.argmin(np.abs(self.y_coords - self.start_coordinate.y))])
+
+            self.current_position = ChipCoordinate(
+                x=self.x_coords[self.current_idx[0]],
+                y=self.y_coords[self.current_idx[1]],
+                z=self.start_coordinate.z)
+
+    def next_waypoint(self) -> list:
+        """
+        Generates iterativly the next waypoint in the given potential field.
+
+        It calculates an infinite number of waypoints with the last waypoints being the actual destination.
+
+        Returns
+        -------
+        Returns a 2D coordinate as next potential field waypoint.
+        """
+        distance_to_target = np.hypot(
+            self.current_position.x - self.target_position.x,
+            self.current_position.y - self.target_position.y)
+
+        if distance_to_target <= self.grid_size:
+            self.current_position = self.target_position
+        else:
+            self.current_idx = self._find_lowest_potential()
+
+            self.current_position = ChipCoordinate(
+                x=self.x_coords[self.current_idx[0]],
+                y=self.y_coords[self.current_idx[1]],
+                z=self.start_coordinate.z)
+
+        return self.current_position
+
+    def set_stage_obstacles(self, *
+                            calibrations: List[Type["Calibration"]]) -> None:
+        """
+        Creates an obstacle in the potential field for each passed calibration.
+
+        Parameters
+        ----------
+        calibrations : List[Calibration]
+            List of calibration instances
+        """
+        self.potential_field = np.zeros_like(
+            self.cx) + self.attractive_potenial_field
+
+        for calibration in calibrations:
+            with calibration.perform_in_chip_coordinates():
+                stage_mask = calibration.stage_polygon.stage_in_meshgrid(
+                    calibration.get_position(),
+                    self.cx,
+                    self.cy)
+
+            for ox, oy in zip(self.cx[stage_mask], self.cy[stage_mask]):
+                o_dist = np.hypot(self.cx - ox, self.cy - oy)
+                field_mask = o_dist < 5 * self.FIBER_RADIUS
+                rep_field = self.REPULSIVE_GAIN * \
+                    (1.0 / o_dist - 1.0 / self.FIBER_RADIUS) ** 2
+
+                self.potential_field[field_mask] += rep_field[field_mask]
+
+    def _find_lowest_potential(self) -> np.ndarray:
+        """
+        Finds lowest potential around current position.
+
+        Returns
+        -------
+        Returns the index of the lowest potential.
+        """
+        curr_potenial = self.potential_field[self.current_idx[1],
+                                             self.current_idx[0]]
+        curr_idx = self.current_idx.copy()
+
+        for mx, my in self.MOTIONS:
+            possible_idx = self.current_idx + np.array([mx, my])
+            possible_potential = self.potential_field[possible_idx[1],
+                                                      possible_idx[0]]
+            if possible_potential < curr_potenial:
+                curr_potenial = possible_potential
+                curr_idx = possible_idx
+
+        return curr_idx
