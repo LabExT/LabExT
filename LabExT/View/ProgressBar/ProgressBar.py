8cc4c4d163a1dc62cfe5fea1b3c2162c4b2e5880
Marco Eppenberger <marco.eppenberger@ief.ee.ethz.ch>
2022-06-01 12:33:39 +0200

    ProgressBar.py: TKinter 8.6 is buggy on MacOS for showing borderless windows - add workaround to have the progressbar w/ border on MacOS...

--- LabExT/View/ProgressBar/ProgressBar.py
+++ LabExT/View/ProgressBar/ProgressBar.py
@@ -8,6 +8,8 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 import _tkinter
 from tkinter import Toplevel, ttk, Label, StringVar
 
+import platform
+
 
 class ProgressBar(Toplevel):
     def __init__(self, root, text):
@@ -27,8 +29,10 @@ class ProgressBar(Toplevel):
                     textvariable=self.text)
         lbl.grid(row=0, column=0)
 
-        # don't show classical window bar for progress bar
-        self.overrideredirect(True)
+        # do not disable window border for MacOS w/ TKinter 8.6 as tkinter is buggy
+        if 'darwin' not in platform.system().lower():
+            # don't show classical window bar for progress bar
+            self.overrideredirect(True)
 
         # this lets the window manager draw all windows, which is necessary as
         while self.root.dooneevent(_tkinter.ALL_EVENTS | _tkinter.DONT_WAIT):
