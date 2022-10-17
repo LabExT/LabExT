3d6346254dda8b27cae6a69cf55428a4e6cd61de
Malte Wächter <git@malte-waechter.com>
2022-10-17 11:54:37 +0200

    Remove testing print statements

--- LabExT/Movement/Calibration.py
+++ LabExT/Movement/Calibration.py
@@ -337,8 +337,6 @@ class Calibration:
         """
         try:
             self._axes_rotation.update(chip_axis, direction, stage_axis)
-            print(
-                f"Calibration {str(self)} Axes Rotation update: {vars(self._axes_rotation)}")
         finally:
             self.determine_state(skip_connection=True)
             self.mover.update_main_model()
@@ -356,8 +354,6 @@ class Calibration:
         """
         try:
             self._single_point_offset.update(pairing)
-            print(
-                f"Calibration {str(self)} SP update: {vars(self._single_point_offset)}")
         finally:
             self.determine_state(skip_connection=True)
             self.mover.update_main_model()
@@ -374,8 +370,6 @@ class Calibration:
         """
         try:
             self._kabsch_rotation.update(pairing)
-            print(
-                f"Calibration {str(self)} Kabsch update: {vars(self._kabsch_rotation)}")
         finally:
             self.determine_state(skip_connection=True)
             self.mover.update_main_model()
