d29ea15231770bd90f3d2d913ddfaeabaedc0435
delmedico <42550680+delmedico@users.noreply.github.com>
2023-02-13 07:25:10 +0100

    Bugfix/killable thread (#146)
    * remove deprecated method isAlive() and simplified
    
    ---------
    
    Co-authored-by: delmedico <>

--- LabExT/Model/Extensions/KillableThread.py
+++ LabExT/Model/Extensions/KillableThread.py
@@ -6,7 +6,7 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 """
 
 import ctypes
-from threading import Thread, _active
+from threading import Thread
 
 
 class KillableThread(Thread):
@@ -15,30 +15,17 @@ class KillableThread(Thread):
     """
 
     def __init__(self, *args, **kwargs):
-        super(KillableThread, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.daemon = True
-
-    def __async_raise__(self, tid, excobj):
-        # the following line calls directly into the C api and needs their types casted to apropriate objects
-        # python3.6 must use ctypes.c_long(tid), whereas python3.7 must use ctypes.c_ulong(tid)!
-        # since we mandate 3.7 for LabExT, we're using the ulong here
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_ulong(tid), ctypes.py_object(excobj))
-        if res == 0:
-            raise ValueError('nonexistent thread id')
-        elif res > 1:
-            """if it returns a number greater than one, you're in trouble,
-            and you should call it again with exc=NULL to revert the effect"""
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-            raise SystemError('PyThreadState_SetAsyncExc failed')
-
-    def raise_exc(self, excobj):
-        if self.isAlive():
-            for tid, tobj in _active.items():
-                if tobj is self:
-                    self.__async_raise__(tid, excobj)
-                    return
 
     def terminate(self):
         """must raise the SystemExit type, instead of a SystemExit() instance
         due to a bug in PyThreadState_SetAsyncExc"""
-        self.raise_exc(SystemExit)
+        if not self.is_alive():
+            return
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_ulong(self.ident), ctypes.py_object(SystemExit))
+        if res == 0:
+            raise ValueError('Invalid thread ID')
+        elif res != 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(self.ident, 0)
+            raise SystemError('PyThreadState_SetAsyncExc failed')
