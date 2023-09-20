88c0ec97ca1d650e08f8b6bb331e09d0af373bfd
Dillon Elste <delste@student.ethz.ch>
2023-09-20 12:23:36 +0900

    Updating EditMeasurementWizard classes to use new architecture
    The controller now has a list of WizardEntryControllers defined by a factory method.
    The sub-controllers are responsible for updating and populating the gui.
    
    In the main controller the way serialization works has now been changed as well.
    The stages will store their contents in a dict, which the main controller will store on disk.
    The views should not interact with disk anyway.

---
+++ LabExT/View/EditMeasurementWizard/WizardEntry/FinishedError.py
@@ -0,0 +1,9 @@
+"""
+LabExT  Copyright (C) 2021  ETH Zurich and Polariton Technologies AG
+This program is free software and comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file.
+"""
+
+class WizardFinishedError(Exception):
+    """This error is raised when the measurement is added to the queue.
+    """
+    pass
