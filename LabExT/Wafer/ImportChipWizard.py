a42c476b18adff4d2d91f80adc27bc827af65500
Marco Eppenberger <meppenberger@psiquantum.com>
2023-12-21 23:08:54 -0800

    Implemented imported devices overview table

--- LabExT/Wafer/ImportChipWizard.py
+++ LabExT/Wafer/ImportChipWizard.py
@@ -6,7 +6,8 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 """
 
 from typing import TYPE_CHECKING
-from tkinter import LEFT, RIGHT, Button, Label, TOP, X, OptionMenu, StringVar
+from tkinter import BOTH, LEFT, RIGHT, Y, Button, Label, TOP, X, OptionMenu, StringVar
+from LabExT.View.Controls.DeviceTable import DeviceTable
 
 from LabExT.View.Controls.Wizard import Wizard, Step
 
@@ -14,10 +15,12 @@ if TYPE_CHECKING:
     from tkinter import Tk
     from LabExT.ExperimentManager import ExperimentManager
     from LabExT.View.Controls.CustomFrame import CustomFrame
+    from LabExT.Wafer.Chip import Chip
 else:
     Tk = None
     ExperimentManager = None
     CustomFrame = None
+    Chip = None
 
 
 class ImportChipWizard(Wizard):
@@ -27,7 +30,7 @@ class ImportChipWizard(Wizard):
             width=800,
             height=600,
             on_cancel=None,
-            on_finish=None,
+            on_finish=self._on_finish,
             with_sidebar=True,
             with_error=True,
             next_button_label="Next Step",
@@ -39,16 +42,28 @@ class ImportChipWizard(Wizard):
 
         self.title("Import Chip")
 
+        self.submitted_chip: Chip = None
+
         self.step_chip_source_selection = ChipSourceSelection(self)
 
         # load all available source steps
         self.source_config_steps_insts = {
             name: _class(wizard=self) for name, _class in self.experiment_manager.chip_source_api.chip_sources.items()
         }
+
+        self.step_final_overview = ShowChipImportResult(self)
+
+        # connect steps
         for step in self.source_config_steps_insts.values():
             step.previous_step = self.step_chip_source_selection
+            step.next_step = self.step_final_overview
 
         self.current_step = self.step_chip_source_selection
+
+    def _on_finish(self):
+        # todo: do some checks on the submitted Chip
+        # then load new chip into experiment manager which should update everything else (mover, mainwindow etc)
+        pass
 
 
 class ChipSourceSelection(Step):
@@ -77,4 +92,16 @@ class ChipSourceSelection(Step):
     def _on_next(self):
         next_step_name = self.source_options_sel_var.get()
         self.next_step = self.wizard.source_config_steps_insts[next_step_name]
+        self.wizard.step_final_overview.previous_step = self.next_step
         return True
+    
+class ShowChipImportResult(Step):
+    def __init__(self, wizard) -> None:
+        super().__init__(wizard, builder=self.build, title="Overview over Imported Devices")
+        self.finish_step_enabled = True
+
+    def build(self, frame: CustomFrame): 
+        Label(frame, text=f"Chip name: {self.wizard.submitted_chip.name:s}").pack(side=TOP, fill=BOTH)
+        dev_table = DeviceTable(frame, self.wizard.submitted_chip)
+        # todo - find way to make device table nice and big
+        dev_table.pack(side=TOP, fill=BOTH)
