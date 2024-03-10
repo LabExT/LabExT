26769576e80eb7ea95115ba8488f25da830425ba
Marco Eppenberger <meppenberger@psiquantum.com>
2024-03-09 18:07:27 -0800

    Added letters to example IDs in the example manifest files

--- LabExT/Tests/Wafer/Chip_import_test.py
+++ LabExT/Tests/Wafer/Chip_import_test.py
@@ -7,7 +7,7 @@ This program is free software and comes with ABSOLUTELY NO WARRANTY; for details
 
 from os.path import dirname, join
 from unittest.mock import Mock
-from flaky import flaky
+import pytest
 
 from LabExT.Tests.Utils import TKinterTestCase
 from LabExT.Utils import setup_user_settings_directory
@@ -17,7 +17,6 @@ from LabExT.Wafer.ChipSources.PhoenixPhotonics import PhoenixPhotonics
 from LabExT.Wafer.ImportChipWizard import ImportChipWizard
 
 
-@flaky(max_runs=3)
 class ChipImportTest(TKinterTestCase):
 
     def setUp(self) -> None:
@@ -34,11 +33,13 @@ class ChipImportTest(TKinterTestCase):
     def setup_window(self):
         return ImportChipWizard(master=self.root, experiment_manager=self.expm)
 
+    @pytest.mark.flaky(reruns=3)
     def test_create_window_instantiate_steps(self):
         window = self.setup_window()
         self.assertIsInstance(window.source_config_steps_insts["IBMMaskDescription"], IBMMaskDescription)
         self.assertIsInstance(window.source_config_steps_insts["PhoenixPhotonics"], PhoenixPhotonics)
 
+    @pytest.mark.flaky(reruns=3)
     def test_IBM_format(self):
 
         window = self.setup_window()
@@ -61,9 +62,12 @@ class ChipImportTest(TKinterTestCase):
         self.assertEqual(window.submitted_chip.name, "chip_test_IBM_format")
         self.assertEqual(window.submitted_chip.path, chip_desc_file_path)
 
-        for _id in range(30):
+        for _id in range(27):
             self.assertIn(str(_id), window.submitted_chip.devices)
         self.assertEqual(len(window.submitted_chip.devices), 30)
+        self.assertIn("27a", window.submitted_chip.devices)
+        self.assertIn("28b1", window.submitted_chip.devices)
+        self.assertIn("29", window.submitted_chip.devices)
 
         self.assertEqual(window.submitted_chip.devices["3"].type, "cutback")
         self.assertAlmostEqual(window.submitted_chip.devices["3"].parameters["length"], 10857.3)
@@ -72,11 +76,13 @@ class ChipImportTest(TKinterTestCase):
         self.assertEqual(window.submitted_chip.devices["29"].type, "cutback")
         self.assertAlmostEqual(window.submitted_chip.devices["29"].parameters["length"], 30000)
 
+
         window._finish_button.invoke()
         self.pump_events()
 
         self.expm.register_chip.assert_called_once_with(window.submitted_chip)
 
+    @pytest.mark.flaky(reruns=3)
     def test_Phoenix_format(self):
 
         window = self.setup_window()
@@ -99,8 +105,11 @@ class ChipImportTest(TKinterTestCase):
         self.assertEqual(window.submitted_chip.name, "chip_test_Phoenix_format")
         self.assertEqual(window.submitted_chip.path, chip_desc_file_path)
 
-        for _id in range(100, 149):
+        for _id in range(100, 146):
             self.assertIn(str(_id), window.submitted_chip.devices)
+        self.assertIn("146a", window.submitted_chip.devices)
+        self.assertIn("147b1", window.submitted_chip.devices)
+        self.assertIn("148", window.submitted_chip.devices)
 
         self.assertEqual(len(window.submitted_chip.devices), 49)
 
@@ -112,6 +121,7 @@ class ChipImportTest(TKinterTestCase):
 
         self.assertAlmostEqual(window.submitted_chip.devices["125"].in_position[0], -1451.25)
         self.assertAlmostEqual(window.submitted_chip.devices["143"].out_position[1], -1287.5)
+
 
         window._finish_button.invoke()
         self.pump_events()
